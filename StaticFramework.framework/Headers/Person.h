cc132828231f6c9ce11e4a0b6bd90cd28fedae37
学而思 <qiaoming@100tal.com>
2018-08-18 19:16:23 +0800

    1111

---
+++ StaticFramework.framework/Headers/Person.h
@@ -0,0 +1,14 @@
+//
+//  Person.h
+//  StaticFramework
+//
+//  Created by 学而思 on 2018/8/13.
+//  Copyright © 2018年 智康. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+@property (nonatomic, strong) NSString *name;
+-(void)test;
+@end
---
+++ StaticFramework.framework/Headers/StaticFramework.h
@@ -0,0 +1,20 @@
+//
+//  StaticFramework.h
+//  StaticFramework
+//
+//  Created by 学而思 on 2018/8/13.
+//  Copyright © 2018年 智康. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Person.h"
+#import "AFNetworking.h"
+//! Project version number for StaticFramework.
+FOUNDATION_EXPORT double StaticFrameworkVersionNumber;
+
+//! Project version string for StaticFramework.
+FOUNDATION_EXPORT const unsigned char StaticFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StaticFramework/PublicHeader.h>
+
+
